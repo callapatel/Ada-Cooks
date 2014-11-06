768c99fe21fe0a90a7653a6938a658df0625c462
Calla <callapatel@gmail.com>
2014-11-06 12:01:30 -0800

    user can create, edit, delete a recipe

--- calla/cooks/app/controllers/recipes_controller.rb
+++ calla/cooks/app/controllers/recipes_controller.rb
@@ -1,4 +1,38 @@
 class RecipesController < ApplicationController
 
+#create, edit, or delete a Recipe
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(params.require(:recipe).permit(:name, :instructions, :prep, :cook))
+    if @recipe.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe = Recipe.update(params.require(:recipe).permit(:name, :instructions, :prep, :cook))
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @recipe = Recipe.all.find(params[:id])
+    @recipe.destroy
+    redirect_to root_path
+  end
+
 
 end
