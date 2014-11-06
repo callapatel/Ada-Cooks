04da69601ddb45a2749cba760d42cbcfd90fa923
Calla <callapatel@gmail.com>
2014-11-06 12:21:33 -0800

    As a guest, I can create or edit an Ingredient

--- calla/cooks/app/controllers/ingredients_controller.rb
+++ calla/cooks/app/controllers/ingredients_controller.rb
@@ -1,3 +1,28 @@
 class IngredientsController < ApplicationController
 
+  def new
+    @ingredient = Ingredient.new
+  end
+
+  def create
+    @ingredient = Ingredient.new(params.require(:ingredient).permit(:name, :quantity))
+    if @ingredient.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @ingredient = Ingredient.find(params[:id])
+  end
+
+  def update
+    @ringredient = Ingredient.find(params[:id])
+    if @ingredient = Ingredient.update(params.require(:ingredient).permit(:name, :quantity))
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
 end
